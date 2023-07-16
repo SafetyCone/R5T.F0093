99b3579ddfd06323b74369f648369f2d37c85ce4
davidcoats <david@getsafetycone.com>
2023-07-16 16:24:39 -0600

    Interim changes.

--- source/R5T.F0093/Code/Functionality/IRepositoryOperations.cs
+++ source/R5T.F0093/Code/Functionality/IRepositoryOperations.cs
@@ -77,7 +77,7 @@ namespace R5T.F0093
         {
             F0080.RepositoryOperations.Instance.Checkin(
                 repositoryContext.LocalDirectoryPath,
-                Instances.CommitMessages.InitialCommit);
+                Instances.CommitMessages.InitialCommit.Value);
 
             return Task.CompletedTask;
         }
